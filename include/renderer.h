5947b0d3607897073825c89f179cdbd5bb16ad2a
shaw <shawwian@gmail.com>
2022-06-27 23:56:40 -0500

    blit_scaled

--- include/renderer.h
+++ include/renderer.h
@@ -7,6 +7,7 @@ void renderer_prepare(void);
 void renderer_present(void);
 SDL_Texture *load_texture(char *filename);
 void blit(SDL_Texture *texture, int x, int y);
+void blit_scaled(SDL_Texture *texture, int x, int y, int w, int h);
 void blit_rect(SDL_Texture *texture, SDL_Rect *src, int x, int y);
 
 #endif
